d88b71a62930c1eb066d94ac5ea4e20de6152d4e
raunakpurohit <raunak.wscubetech@gmail.com>
2026-03-14 20:02:41 +0530

    post api implemented future builder

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -5,13 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class PostPage extends StatefulWidget {
-
   @override
   State<PostPage> createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
-  List<PostModel> mPosts = [];
 
   @override
   void initState() {
@@ -19,57 +17,92 @@ class _PostPageState extends State<PostPage> {
     getPosts();
   }
 
-  getPosts() async{
+  Future<List<PostModel>> getPosts() async {
     String url = "https://dummyjson.com/posts";
 
-    http.Response res = await  http.get(Uri.parse(url));
-
-    if(res.statusCode==200){
+    http.Response res = await http.get(Uri.parse(url));
+    List<PostModel> mPosts = [];
+    if (res.statusCode == 200) {
       dynamic rawData = jsonDecode(res.body);
       DataModel dataModel = DataModel.fromJson(rawData);
       mPosts = dataModel.posts;
-      setState(() {
-
-      });
+      return mPosts;
+    } else {
+      return [];
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Posts'),
-      ),
-      body: mPosts.isNotEmpty ? Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(itemBuilder: (_, index){
-          return Card(
-            child: ListTile(
-              title: Text(mPosts[index].title),
-              subtitle: Text(mPosts[index].body, maxLines: 2, overflow: TextOverflow.ellipsis,),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.thumb_up_alt_outlined, color: Colors.green,),
-                      Text(mPosts[index].reactions.likes.toString()),
-                    ],
-                  ),
-                  SizedBox(width: 10,),
-                  Column(
-                    children: [
-                      Icon(Icons.thumb_down_alt_outlined, color: Colors.red,),
-                      Text(mPosts[index].reactions.dislikes.toString()),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        }),
-      ) : Center(
-        child: Text('No post yet!!'),
+      appBar: AppBar(title: Text('Posts')),
+      body: FutureBuilder<List<PostModel>>(
+        future: getPosts(),
+        builder: (_, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snap.hasData) {
+            List<PostModel> mPosts = snap.data!;
+            return mPosts.isNotEmpty
+                ? Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemBuilder: (_, index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(mPosts[index].title),
+                            subtitle: Text(
+                              mPosts[index].body,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Column(
+                                  children: [
+                                    Icon(
+                                      Icons.thumb_up_alt_outlined,
+                                      color: Colors.green,
+                                    ),
+                                    Text(
+                                      mPosts[index].reactions.likes.toString(),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(width: 10),
+                                Column(
+                                  children: [
+                                    Icon(
+                                      Icons.thumb_down_alt_outlined,
+                                      color: Colors.red,
+                                    ),
+                                    Text(
+                                      mPosts[index].reactions.dislikes
+                                          .toString(),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Center(child: Text('No post yet!!'));
+          }
+
+          if(snap.hasError){
+            return Center(
+              child: Text('Error: ${snap.error}'),
+            );
+          }
+
+          return Container();
+        },
       ),
     );
   }
